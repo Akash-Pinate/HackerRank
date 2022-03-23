c6e21c4e5c9079b3203de3fb8a00499ffc3a834c
Akash-Pinate <78008539+Akash-Pinate@users.noreply.github.com>
2022-03-23 13:52:11 +0530

    Create Sum_of_Digit.c

---
+++ C/Sum of Digit of a Five Number/Sum_of_Digit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+//#include <string.h>
+//#include <math.h>
+//#include <stdlib.h>
+
+int SumDigit(int iNo)
+{
+    int remainder=0;
+    int iSum=0;
+    
+    while(iNo!=0)
+    {
+        remainder=iNo%10;
+        iSum=iSum+remainder;
+        iNo=iNo/10;
+    }
+    
+    return iSum;
+}
+
+
+int main() {
+	
+    int n;
+    int iRet=0;
+    
+    scanf("%d", &n);
+    
+    iRet=SumDigit(n);
+    printf("%d",iRet);
+    
+    return 0;
+}
